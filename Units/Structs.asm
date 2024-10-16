c370d42add92e284b5a55050de0b2a0507c79062
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-16 11:10:42 +0300

    add collide types

--- Units/Structs.asm
+++ Units/Structs.asm
@@ -52,6 +52,46 @@ struct Object
   drawing     Drawing
   animation   Animation
 ends
+
+DECORATION        = 1
+
+;blocks
+;both
+BLOCK             = DECORATION * 2
+JUMP              = BLOCK * 2
+TELEPORT          = JUMP * 2
+DELETE            = TELEPORT * 2
+;player
+TOP_BLOCK         = DELETE * 2
+TOP_BREAK         = TOP_BLOCK * 2
+BOTTOM_LUCK       = TOP_BREAK * 2
+BOTTOM_BREAK      = BOTTOM_LUCK * 2
+;enemy
+REVERSE           = BOTTOM_BREAK * 2
+ROTATE            = REVERSE * 2
+
+;bonuses 
+COIN              = ROTATE * 2
+STAR              = COIN * 2
+HEART             = STAR * 2
+POTION            = HEART * 2
+AQUA              = POTION * 2
+
+;enemies
+;with player
+ENEMY             = AQUA * 2
+UNTOCHABLE_ENEMY  = ENEMY * 2 
+UNBEATABLE_ENEMY  = UNTOCHABLE_ENEMY * 2
+;with block
+DEAD_ENEMY        = UNBEATABLE_ENEMY * 2
+DECORATIVE_ENEMY  = DEAD_ENEMY
+JUMPABLE_ENEMY    = DECORATIVE_ENEMY * 2
+REVERSEABLE_ENEMY = JUMPABLE_ENEMY * 2
+ROTATEABLE_ENEMY  = REVERSEABLE_ENEMY * 2
+
+;players
+PLAYER            = ROTATEABLE_ENEMY * 2
+PLAYER_BULLET     = PLAYER * 2
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
