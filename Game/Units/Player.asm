6d21cc0cb661ed2273f060ff0ed0bbac76d3f13e
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-23 09:44:25 +0300

    move struct Player

---
+++ Game/Units/Player.asm
@@ -0,0 +1,15 @@
+;-------------------------------------------------------------------------------
+struct Player
+  entity                  Entity
+  canJump                 dd ?
+  totalBullets            dd ?
+  refsBullets             dd ?
+  hasHeart                dd ?
+  hasArrow                dd ?
+  hasWorld                dd ?
+  maxWorldTimer           dd ?
+  worldTimer              dd ?
+  maxInvulnerabilityTimer dd ?
+  invulnerabilityTimer    dd ?  
+ends
+;-------------------------------------------------------------------------------
