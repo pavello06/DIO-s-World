dac90c2c2cad4f4b53950709a9f908b6794d66f5
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-23 09:51:58 +0300

    add player

--- Game/Units/Player.asm
+++ Game/Units/Player.asm
@@ -3,7 +3,7 @@ struct Player
   entity                  Entity
   canJump                 dd ?
   totalBullets            dd ?
-  refsBullets             dd ?
+  refBullets              dd ?
   hasHeart                dd ?
   hasArrow                dd ?
   hasWorld                dd ?
@@ -12,4 +12,13 @@ struct Player
   maxInvulnerabilityTimer dd ?
   invulnerabilityTimer    dd ?  
 ends
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+player Player <<<<<Structs.GAME, 30, 100, 1, 1>,\ 
+              Structs.PLAYER>,\
+              <Structs.NORMAL, Structs.RIGHT, Structs.UP, standingPlayerTexture>>,\
+              <FALSE, 200, 0, 0, standingPlayerFrames>>,\
+              0, 0, TRUE>,\
+              FALSE, 3, 0, FALSE, FALSE, FALSE, 5000, -1, 2000, -1
 ;-------------------------------------------------------------------------------
