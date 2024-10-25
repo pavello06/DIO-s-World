cb70928059c4be37688ca2d889e5151980ca020b
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-25 22:56:49 +0300

    add proc Delete

--- Units/Object.asm
+++ Units/Object.asm
@@ -9,3 +9,14 @@ ends
 Object.GENERAL = 0
 Object.MENU    = 1
 Object.GAME    = 2
+
+proc Object.Delete\
+     refObject
+
+        mov     eax, [refObject]
+        
+        mov     DWORD [eax + Object.x], -1000
+        mov     DWORD [eax + Object.y], -1000
+
+        ret
+endp
