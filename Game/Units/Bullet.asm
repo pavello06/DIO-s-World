8877e81e63372c3e528a692e35334df24573b1eb
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-27 11:37:36 +0300

    add procs Activate Deactivate

--- Game/Units/Bullet.asm
+++ Game/Units/Bullet.asm
@@ -3,16 +3,41 @@ struct Bullet
   isActive dd ?
 ends
 
-proc Bullet.Activate\
-     refBullet
+proc Bullet.ActivateOrDeactivate
+     refBullet, refRefsFrames, speedX, speedY , isActive
      
         mov     eax, [refBullet]
+        mov     ecx, [refRefsFrames]
         
-        ;анимацию
+        mov     DWORD [eax + GameObjectWithAnimation.animation.isFinite], TRUE
+        mov     DWORD [eax + GameObjectWithAnimation.animation.currentFrame], 0
+        mov     DWORD [eax + GameObjectWithAnimation.animation.refsFrames], ecx
         
-        mov     DWORD [eax + Entity.speedX], 0
-        mov     DWORD [eax + Entity.speedY], 0
-        mov     DWORD [eax + Bullet.isActive], TRUE
+        mov     ecx, [speedX]
+        mov     DWORD [eax + Entity.speedX], ecx
+        mov     ecx, [speedY]
+        mov     DWORD [eax + Entity.speedY], ecx
+        mov     ecx, [isActive]
+        mov     DWORD [eax + Bullet.isActive], ecx
+        
+        add     eax, sizeof.GameObjectWithDrawing 
+        stdcall Animation.Start, eax
+     
+        ret
+endp        
+
+proc Bullet.Activate\
+     refBullet, refRefsFrames
+     
+        stdcall Bullet.ActivateOrDeactivate, [refBullet], [refRefsFrames], 0, 0, TRUE
+     
+        ret
+endp
+
+proc Bullet.Deactivate\
+     refBullet, refRefsFrames, speedX, speedY
+     
+        stdcall Bullet.ActivateOrDeactivate, [refBullet], [refRefsFrames], [speedX], [speedY], FALSE
      
         ret
 endp
