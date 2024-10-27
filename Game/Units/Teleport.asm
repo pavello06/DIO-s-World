778a29c96b8cee0adf7b41331c10ebb255b2d708
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-27 13:40:13 +0300

    add proc TeleportObject

--- Game/Units/Teleport.asm
+++ Game/Units/Teleport.asm
@@ -3,3 +3,17 @@ struct Teleport
   x          dd ?
   y          dd ?
 ends
+
+proc Teleport.TeleportObject
+     refObject, refTeleport
+     
+        mov     eax, [refObject]
+        mov     ecx, [refTeleport]
+        
+        mov     edx, [ecx + Teleport.x]
+        mov     [eax + Object.x], edx
+        mov     edx, [ecx + Teleport.y]
+        mov     [eax + Object.y], edx
+     
+        ret     
+endp
