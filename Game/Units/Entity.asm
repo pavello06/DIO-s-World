6551c65a724803eaa528fdf16c928adb8f7f8d95
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-27 11:43:10 +0300

    add proc Delete

--- Game/Units/Entity.asm
+++ Game/Units/Entity.asm
@@ -4,3 +4,17 @@ struct Entity
   speedY                  dd ?
   canGravitate            dd ?
 ends
+
+proc Entity.Delete\
+     refEntity
+     
+        stdcall Object.Delete, [refEntity]
+        
+        mov     eax, [refEntity]
+        
+        mov     DWORD [eax + Entity.speedX], 0
+        mov     DWORD [eax + Entity.speedY], 0
+        mov     DWORD [eax + Entity.canGravitate], FALSE
+     
+        ret
+endp
