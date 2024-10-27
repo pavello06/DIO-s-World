9f32917d085577742f185558a25e6005449f0be9
Pavel Halukha <pavelgaluha@gmail.com>
2024-10-27 14:16:35 +0300

    edit proc GetDamage

--- Game/Units/Enemy.asm
+++ Game/Units/Enemy.asm
@@ -18,8 +18,6 @@ proc Enemy.GetDamage\
         add     edx, eax   
         mov     eax, [edx + Animation.isFinite]
         mov     [ecx + GameObjectWithAnimation.animation.isFinite], eax
-        mov     eax, [edx + Animation.timer]
-        mov     [ecx + GameObjectWithAnimation.animation.timer], eax
         mov     eax, [edx + Animation.maxTimer]
         mov     [ecx + GameObjectWithAnimation.animation.maxTimer], eax
         mov     eax, [edx + Animation.currentFrame]
@@ -27,10 +25,15 @@ proc Enemy.GetDamage\
         mov     eax, [edx + Animation.refFrames]
         mov     [ecx + GameObjectWithAnimation.animation.refFrames], eax
         
-        cmp     DWORD [ecx + Enemy.health], 0
+        add     ecx, sizeof.GameObjectWithDrawing
+        stdcall Animation.Start, ecx
+        
+        mov     eax, [refEnemy]       
+        
+        cmp     DWORD [eax + Enemy.health], 0
         jne     .exit
         
-        stdcall Enemy.Die, ecx
+        stdcall Enemy.Die, eax
         
   .exit:   
         ret
