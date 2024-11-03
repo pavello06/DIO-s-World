b8c93c584c52a61233b58415007de8371768a126
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-03 18:20:24 +0300

    add proc SpawnBonus

--- Game/Units/Luck.asm
+++ Game/Units/Luck.asm
@@ -2,3 +2,24 @@ struct Luck
   gameObjectWithAnimation GameObjectWithAnimation
   refBonus                dd ?
 ends
+
+proc Luck.SpawnBonus\
+     refLuck
+     
+        mov     eax, [refLuck]
+        mov     ecx, [eax + Luck.refBonus]
+        
+        mov     edx, [eax + Object.width]
+        sub     edx, [ecx + Object.width]
+        shr     edx, 1
+        add     edx, [eax + Object.x]
+        
+        mov     [ecx + Object.x], edx
+        
+        mov     edx, [eax + Object.y]
+        add     edx, [eax + Object.height]
+        
+        mov     [ecx + Object.y], edx
+     
+        ret 
+endp
