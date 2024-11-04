a56cdf883627c1d79f7793cf21abfdd727108d99
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-04 14:31:02 +0300

    fix Snail.GetDamage

--- Game/Units/Snail.asm
+++ Game/Units/Snail.asm
@@ -3,23 +3,45 @@ struct Snail
   countOfCollides dd ?
 ends
 
-proc Snail.GetDamage\
-     refSnail
+Snail.SNAIL_SPEED_BOOST_X = 20
+
+proc Snail.GetDamage uses ebx,\
+     refSnail, refPlayer
      
         stdcall Enemy.GetDamage, [refSnail]
         
-        mov     eax, [refSnail]
+        mov     ebx, [refSnail]
         
-        cmp     DWORD [eax + Enemy.health], 2
+        cmp     DWORD [ebx + Enemy.health], 2
         jne     .notStop
         
-        mov     DWORD [eax + Enemy.speedX], 0
+        mov     DWORD [ebx + Enemy.speedX], 0
   
   .notStop:        
-        cmp     DWORD [eax + Enemy.health], 1
+        cmp     DWORD [ebx + Enemy.health], 1
         jne     .exit
   
-        mov     DWORD [eax + Enemy.speedX], 20
+        mov     ecx, [refPlayer]
+        mov     edx, [ecx + Object.width]
+        shr     edx, 1
+        add     edx, [ecx + Object.x]
+        
+        mov     ecx, [eax + Object.width]
+        shr     ecx, 1
+        add     ecx, [eax + Object.x]
+        
+        mov     eax, 1
+        
+        cmp     edx, ecx
+        jb      .toTheRight
+        
+  ..toTheLeft:
+        mov     eax, -1
+  
+  .toTheRight:
+        mov     ecx, Snail.SNAIL_SPEED_BOOST_X
+        mul     ecx
+        mov     DWORD [ebx + Enemy.speedX], eax
         
   .exit:   
         ret
