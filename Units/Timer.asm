af2d0e592f3bf0deffbf791d00a573a8d6b6dc3f
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-05 23:11:15 +0300

    add proc Timer.IsTimeUp

---
+++ Units/Timer.asm
@@ -0,0 +1,23 @@
+proc Timer.IsTimeUp\
+     refTimer, maxTimer
+     
+        invoke	GetTickCount
+        
+        mov     ecx, [refTimer]
+        
+	      sub	    eax, [ecx]
+	      cmp	    eax, [maxTimer]
+	      jb	    .TimeIsNotUp
+        
+  .TimeIsUp:
+        add	    [ecx], eax
+        
+        mov     eax, TRUE
+        jmp     .exit   
+  
+  .TimeIsNotUp:
+        mov     eax, FALSE
+  
+  .exit:   
+        ret     
+endp
