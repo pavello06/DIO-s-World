fe0d14ffc2e1d5a12f60675e00d6d61e4403993f
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-12 20:36:53 +0300

    create proc ChangeTo

--- Units/WindowProcFunction/WindowProcFunction.asm
+++ Units/WindowProcFunction/WindowProcFunction.asm
@@ -6,3 +6,18 @@ struct WindowProcFunctions
 ends
 
 windowProcFunctions WindowProcFunctions
+
+proc WindowProcFunctions.ChangeTo\
+     refPaint, refTimer, refKeyDown, refKeyUp
+
+        mov       eax, [refPaint]
+        mov       [windowProcFunctions.refPaint], eax
+        mov       eax, [refTimer]
+        mov       [windowProcFunctions.refTimer], eax
+        mov       eax, [refKeyDown]
+        mov       [windowProcFunctions.refKeyDown], eax
+        mov       eax, [refKeyUp]
+        mov       [windowProcFunctions.refKeyUp], eax
+
+        ret
+endp
