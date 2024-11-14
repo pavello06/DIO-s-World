c87dd95142df737110e4d0778d0c18af13e19ff8
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-14 16:32:46 +0300

    rebuild struct Button

--- Menu/Units/Button/Button.asm
+++ Menu/Units/Button/Button.asm
@@ -1,6 +1,28 @@
 struct Button
   menuObjectWithDrawing MenuObjectWithDrawing
-  type                  dd ?
   isActive              dd ? 
   refAction             dd ?
+  argument              dd ?
 ends
+
+proc Button.Game\
+     refScreen
+
+        stdcall WindowProcFunctions.ChangeToGame
+        
+        mov     eax, [refScreen]
+        mov     [level], eax
+
+        ret
+endp
+
+proc Button.Menu\
+     refScreen
+
+        stdcall WindowProcFunctions.ChangeToMenu
+        
+        mov     eax, [refScreen]
+        mov     [menu], eax
+
+        ret
+endp
