be97952b64516614f671d0f1c02213e21e0d5f48
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-14 21:44:09 +0300

    add proc Array.Iterate

--- Units/Array/Array.asm
+++ Units/Array/Array.asm
@@ -2,3 +2,23 @@ struct Array
   length   dd ?
   elements dd ?
 ends
+
+proc Array.Iterate uses ebx,\
+     refArray, refAction
+     
+        mov     ebx, [refArray] 
+        
+        mov     ecx, [ebx + Array.length]
+        add     ebx, Array.elements
+  
+  .loop:
+        push    ecx     
+        
+        stdcall [refAction], [ebx]
+                                
+        add     ebx, sizeof.Array.elements
+        pop     ecx                                   
+        loop    .loop        
+
+        ret
+endp
