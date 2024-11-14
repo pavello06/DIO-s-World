734d50628559467ac7d4bb924cddca434f962778
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-14 13:02:48 +0300

    success merge

--- Menu/Units/Button/ScreenButton.asm
+++ Menu/Units/Button/ScreenButton.asm
@@ -3,12 +3,24 @@ struct ScreenButton
   refScreen dd ?
 ends
 
-proc Button.Game\
+proc ScreenButton.Game\
      refScreen
 
+        stdcall WindowProcFunctions.ChangeToGame
         
+        mov     eax, [refScreen]
+        mov     [level], eax
 
+        ret
+endp
 
+proc ScreenButton.Menu\
+     refScreen
+
+        stdcall WindowProcFunctions.ChangeToMenu
+        
+        mov     eax, [refScreen]
+        mov     [menu], eax
 
         ret
 endp
