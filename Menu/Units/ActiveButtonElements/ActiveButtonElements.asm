cdaf9be726073adb3d2b526cff49c190bcc65cd3
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-15 21:52:28 +0300

    git add border to active button

--- Menu/Units/ActiveButtonElements/ActiveButtonElements.asm
+++ Menu/Units/ActiveButtonElements/ActiveButtonElements.asm
@@ -1,15 +1,15 @@
 activeButtonElement1 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
-                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
-                                             <FALSE, 0, 100, 0, beeFrames>
+                                             <6, Drawing.RIGHT, Drawing.UP, activeButtonElementTexture>>,\
+                                             <FALSE, 0, 100, 0, activeButtonElementFrames>
                                              
 activeButtonElement2 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
-                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
-                                             <FALSE, 0, 100, 0, beeFrames>
+                                             <6, Drawing.RIGHT, Drawing.UP, activeButtonElementTexture>>,\
+                                             <FALSE, 0, 100, 0, activeButtonElementFrames>
                                              
 activeButtonElement3 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
-                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
-                                             <FALSE, 0, 100, 0, beeFrames>
+                                             <6, Drawing.RIGHT, Drawing.UP, activeButtonElementTexture>>,\
+                                             <FALSE, 0, 100, 0, activeButtonElementFrames>
                                              
 activeButtonElement4 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
-                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
-                                             <FALSE, 0, 100, 0, beeFrames>
+                                             <6, Drawing.RIGHT, Drawing.UP, activeButtonElementTexture>>,\
+                                             <FALSE, 0, 100, 0, activeButtonElementFrames>
