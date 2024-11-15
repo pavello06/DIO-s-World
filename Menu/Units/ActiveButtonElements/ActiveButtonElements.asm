f503c151fb56f6d7ec15fd5c164ea30020a59fec
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-15 21:45:56 +0300

    add borders to active button

---
+++ Menu/Units/ActiveButtonElements/ActiveButtonElements.asm
@@ -0,0 +1,15 @@
+activeButtonElement1 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
+                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
+                                             <FALSE, 0, 100, 0, beeFrames>
+                                             
+activeButtonElement2 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
+                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
+                                             <FALSE, 0, 100, 0, beeFrames>
+                                             
+activeButtonElement3 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
+                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
+                                             <FALSE, 0, 100, 0, beeFrames>
+                                             
+activeButtonElement4 MenuObjectWithAnimation <<<Object.MENU, 0, 0, 1, 1>>,\
+                                             <6, Drawing.RIGHT, Drawing.UP, beeTexture>>,\
+                                             <FALSE, 0, 100, 0, beeFrames>
