d5859a7d799870c3a0de2b3a73ab1f264764e0c0
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-16 15:37:00 +0300

    refact WindowProcFunctions

--- Units/WindowProcFunctions/WindowProcFunctions.asm
+++ Units/WindowProcFunctions/WindowProcFunctions.asm
@@ -1,37 +1,37 @@
 struct WindowProcFunctions
-  refPaint   dd ?
-  refTimer   dd ?
   refKeyDown dd ?
   refKeyUp   dd ?
+  refPaint   dd ?
+  refTimer   dd ?
 ends
 
 windowProcFunctions WindowProcFunctions
 
 proc WindowProcFunctions.ChangeTo\
-     refPaint, refTimer, refKeyDown, refKeyUp
+     refKeyDown, refKeyUp, refPaint, refTimer
 
-        mov       eax, [refPaint]
-        mov       [windowProcFunctions.refPaint], eax
-        mov       eax, [refTimer]
-        mov       [windowProcFunctions.refTimer], eax
         mov       eax, [refKeyDown]
         mov       [windowProcFunctions.refKeyDown], eax
         mov       eax, [refKeyUp]
         mov       [windowProcFunctions.refKeyUp], eax
+        mov       eax, [refPaint]
+        mov       [windowProcFunctions.refPaint], eax
+        mov       eax, [refTimer]
+        mov       [windowProcFunctions.refTimer], eax
 
         ret
 endp
 
 proc WindowProcFunctions.ChangeToGame
 
-        stdcall WindowProcFunctions.ChangeTo, Game.Paint, Game.Timer, Game.KeyDown, Game.KeyUp
+        stdcall WindowProcFunctions.ChangeTo, Game.KeyDown, Game.KeyUp, Game.Paint, Game.Timer
 
         ret
 endp
 
 proc WindowProcFunctions.ChangeToMenu
 
-        stdcall WindowProcFunctions.ChangeTo, Menu.Paint, Menu.Timer, Menu.KeyDown, Menu.KeyUp
+        stdcall WindowProcFunctions.ChangeTo, Menu.KeyDown, Menu.KeyUp, Menu.Paint, Menu.Timer
 
         ret
 endp
