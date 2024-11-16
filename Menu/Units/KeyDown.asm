2075e3b60f97e7e5777b169337ce00f33f7da65e
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-16 16:23:27 +0300

    check bounds

--- Menu/Units/KeyDown.asm
+++ Menu/Units/KeyDown.asm
@@ -27,6 +27,9 @@ proc KeyDown.PreviousAndNext\
   .previous:
         mov     eax, [currentMenu]
         stdcall Button.GetActiveButtonInArray, [eax + Menu.menuObjects.refButtons]
+        cmp     DWORD [eax - 4], 0
+        je      .exit
+        
         mov     ecx, [eax]
         mov     DWORD [ecx + Button.isActive], FALSE
         sub     eax, 4
@@ -44,10 +47,16 @@ proc KeyDown.PreviousAndNext\
   .next:
         mov     eax, [currentMenu]
         stdcall Button.GetActiveButtonInArray, [eax + Menu.menuObjects.refButtons]
+        cmp     DWORD [eax + 4], 0
+        je      .exit
+        
         mov     ecx, [eax]
         mov     DWORD [ecx + Button.isActive], FALSE
         add     eax, 4
         mov     ecx, [eax]
+        cmp     ecx, 0
+        je      .exit
+        
         mov     DWORD [ecx + Button.isActive], TRUE          
         stdcall ActiveButtonElement.ChangeActiveButton, ecx
         
