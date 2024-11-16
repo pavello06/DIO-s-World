c051754d3168ca8db2c3fbab33ce48e101c148d7
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-16 16:24:12 +0300

    add background

--- Menu/Menu.asm
+++ Menu/Menu.asm
@@ -19,7 +19,7 @@ proc Menu.Paint uses ebx esi edi
         mov     edi, [Screen.screen + Object.y]
         add     edi, [Screen.screen + Object.height]
         
-        stdcall Screen.Clear 
+        stdcall Screen.Fill, 80, 187, 255 
         stdcall Drawing.DrawObjects, [ebx + Menu.menuObjects.refMenuObjectsWithDrawing], [Screen.screen + Object.x], esi, [Screen.screen + Object.y], edi
         stdcall Animation.AnimateObjects, [ebx + Menu.menuObjects.refMenuObjectsWithAnimation], [Screen.screen + Object.x], esi, [Screen.screen + Object.y], edi
 
