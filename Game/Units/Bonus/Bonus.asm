6c60e2ef6f0c5588b46200e3e2971bce4de9adf1
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-24 21:47:10 +0300

    add procs of gets bonuses

--- Game/Units/Bonus/Bonus.asm
+++ Game/Units/Bonus/Bonus.asm
@@ -3,8 +3,66 @@ struct Bonus
   type                    dd ?
 ends
 
-Bonus.COIN  = 1
-Bonus.STAR  = 2
-Bonus.HEART = 3
-Bonus.ARROW = 4
-Bonus.WORLD = 5
+Bonus.COIN  = 0
+Bonus.STAR  = 1
+Bonus.HEART = 2
+Bonus.ARROW = 3
+Bonus.WORLD = 4
+
+Bonus.COIN_SCORE = 10
+Bonus.STAR_SCORE = 400
+
+proc Bonus.GetCoin
+     refBonus
+     
+        mov     eax, [currentLevel]
+        
+        add     DWORD [eax + Level.levelStatistics.score], Bonus.COIN_SCORE
+        
+        stdcall Object.Delete, [refBonus]
+
+        ret
+endp
+
+proc Bonus.GetStar
+     refBonus
+     
+        mov     eax, [currentLevel] 
+        
+        add     DWORD [eax + Level.levelStatistics.score], Bonus.STAR_SCORE
+        inc     DWORD [eax + Level.levelStatistics.stars]
+        
+        stdcall Object.Delete, [refBonus]
+
+        ret
+endp
+
+proc Bonus.GetHeart
+     refBonus
+        
+        mov     [player.hasHeart], TRUE
+        
+        stdcall Object.Delete, [refBonus]
+
+        ret
+endp
+
+proc Bonus.GetArrow
+     refBonus
+        
+        mov     [player.hasArrow], TRUE
+        
+        stdcall Object.Delete, [refBonus]
+
+        ret
+endp
+
+proc Bonus.GetWorld
+     refBonus
+     
+        stdcall Timer.Stop, player.worldTimer
+        
+        stdcall Object.Delete, [refBonus]
+
+        ret
+endp
