741835db1a9179da8a54dd8e3e22f24bddfec00a
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-25 18:23:42 +0300

    remove refObjects and use class Array

--- Game/Units/Move/Move.asm
+++ Game/Units/Move/Move.asm
@@ -4,7 +4,7 @@ Move.timer     dd 0
 Move.G = 2
 
 proc Move.MoveEntityCoordinate uses ebx esi edi,\
-     refEntity, refObjects, coordinate
+     refEntity, coordinate
      
         mov     ebx, [refEntity]
         
@@ -26,7 +26,7 @@ proc Move.MoveEntityCoordinate uses ebx esi edi,\
         push    ecx
         
         add     [ebx + esi + Object.x], edi
-        stdcall Collide.HandleCollisions, ebx, [refObjects]
+        stdcall Collide.HandleCollisions, ebx
              
         pop     ecx
         loop    .loop                
@@ -36,24 +36,24 @@ proc Move.MoveEntityCoordinate uses ebx esi edi,\
 endp
 
 proc Move.MoveEntityCoordinateX\
-     refEntity, refObjects
+     refEntity
      
-        stdcall Move.MoveEntityCoordinate, [refEntity], [refObjects], 0
+        stdcall Move.MoveEntityCoordinate, [refEntity], 0
             
         ret
 endp
 
 proc Move.MoveEntityCoordinateY\
-     refEntity, refObjects
+     refEntity
      
-        stdcall Move.MoveEntityCoordinate, [refEntity], [refObjects], sizeof.Object.x
+        stdcall Move.MoveEntityCoordinate, [refEntity], sizeof.Object.x
             
         ret
 endp
      
 
 proc Move.MoveEntity uses ebx,\
-     refEntity, refObjects
+     refEntity
         
         mov     ebx, [refEntity]
         
@@ -67,41 +67,21 @@ proc Move.MoveEntity uses ebx,\
         sub     DWORD [ebx + Entity.speedY], Move.G
         
   .canNotGravitate:      
-        stdcall Move.MoveEntityCoordinateX, ebx, [refObjects]
-        stdcall Move.MoveEntityCoordinateY, ebx, [refObjects]
+        stdcall Move.MoveEntityCoordinateX, ebx
+        stdcall Move.MoveEntityCoordinateY, ebx
 
   .exit:     
         ret
 endp
 
 proc Move.MoveEntities uses ebx,\
-     refEntities, refObjects
+     refEntities
      
         stdcall Timer.IsTimeUp, Move.timer, Move.MAX_TIMER
 	      cmp	    eax, FALSE 
 	      je	    .exit
-     
-        mov     ebx, [refEntities] 
         
-        mov     ecx, [ebx + 0]
-        
-        cmp     [player + Player.worldTimer], -1
-        je      .notWorldTimer
-  
-  .worldTimer:      
-        mov     ecx, 1      
-        
-  .notWorldTimer:      
-        add     ebx, 4
-  
-  .loop:
-        push    ecx      
-        
-        stdcall Move.MoveEntity, [ebx], [refObjects]
-                    
-        add     ebx, 4
-        pop     ecx                                   
-        loop    .loop
+        stdcall Array.Iterate, Move.MoveEntity, [refEntities]
         
   .exit:     
         ret
